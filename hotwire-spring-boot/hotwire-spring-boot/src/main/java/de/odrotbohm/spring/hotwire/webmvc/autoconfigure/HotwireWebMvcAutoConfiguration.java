fc86a28d26ceb4b35438349afecfef8cd098622c
Oliver Drotbohm <odrotbohm@vmware.com>
2021-03-31 18:40:36 +0200

    Only enable Hotwire auto-configuration in Servlet environments.

--- hotwire-spring-boot/hotwire-spring-boot/src/main/java/de/odrotbohm/spring/hotwire/webmvc/autoconfigure/HotwireWebMvcAutoConfiguration.java
+++ hotwire-spring-boot/hotwire-spring-boot/src/main/java/de/odrotbohm/spring/hotwire/webmvc/autoconfigure/HotwireWebMvcAutoConfiguration.java
@@ -24,6 +24,8 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -38,6 +40,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
  */
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication(type = Type.SERVLET)
 class HotwireWebMvcAutoConfiguration implements WebMvcConfigurer {
 
 	private final @NonNull ThymeleafViewResolver resolver;
